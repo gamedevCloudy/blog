89ffed036e9b1dc11908b5b036940e9a4c15f729
Aayush Chaudhary <acaayush1111@gmail.com>
2024-09-03 11:49:51 +0530

    feat: upadte freezer

--- freeze.py
+++ freeze.py
@@ -1,8 +1,18 @@
-from app import app
+from app import app, load_posts
 from flask_frozen import Freezer
 
-
+# Initialize the Freezer with your Flask app
 freezer = Freezer(app)
+
+# Register a generator for dynamic routes (if any)
+@freezer.register_generator
+def go_to_blog():
+    # Load all posts
+    posts = load_posts()
+    # Yield all dynamic URLs for posts
+    for post in posts:
+        # Assumes the 'permalink' is part of the URL for the post
+        yield {'post_name': post['permalink'].split('/')[-1]}
 
 if __name__ == '__main__':
     freezer.freeze()
